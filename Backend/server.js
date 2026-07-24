d0ea24f2543401e5b34c736825621ab657a6e87f
Vispok <visheshpokhriyal598@gmail.com>
2026-07-24 10:56:17 +0530

    Describe what you changed

---
+++ Backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.use(cors({
+    origin: "http://localhost:3000"
+}))
+app.listen(port,()=>{
+    console.log(`Server is Running on http://localhost:${port}`);
+})
