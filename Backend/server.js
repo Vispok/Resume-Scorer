72185bbff46ec1889fd9fabc7d942835e3d1302b
Vispok <visheshpokhriyal598@gmail.com>
2026-07-26 22:49:05 +0530

    Set up Express backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,20 +2,39 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
+import multer from "multer";
+import axios from "axios";
+import FormData from "form-data";
+import fs from "fs";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+const upload = multer({dest :"uploads/"})
+const form = new FormData();
 
 app.use(morgan("combined"));
+
+form.append(
+    "resume",
+    fs.createReadStream(req.file.path)
+);
+
+const response = await axios.post(
+    "http://localhost:5000/extract-skills",
+    form,
+    {
+        headers: form.getHeaders()
+    }
+);
 
 app.use(cors({
     origin: "http://localhost:3000"
 }))
 
-app.post("/submit",(req,res)=>{
-    
+app.post("/upload",upload.single("resume"),async(req,res)=>{
+    console.log(req.file)
 })
 
 app.get("/",(req,res)=>{
