c40d59a86c9da4a77794b110b2973130f4df2923
Vispok <visheshpokhriyal598@gmail.com>
2026-07-27 20:03:28 +0530

    Set up Express backend

--- Backend/server.js
+++ Backend/server.js
@@ -6,35 +6,69 @@ import multer from "multer";
 import axios from "axios";
 import FormData from "form-data";
 import fs from "fs";
+import { findPackageJSON } from "module";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 const upload = multer({dest :"uploads/"})
-const form = new FormData();
 
 app.use(morgan("combined"));
 
-form.append(
-    "resume",
-    fs.createReadStream(req.file.path)
-);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-const response = await axios.post(
-    "http://localhost:5000/extract-skills",
-    form,
-    {
-        headers: form.getHeaders()
-    }
-);
+//To talk with Frontend
 
 app.use(cors({
     origin: "http://localhost:3000"
 }))
 
+//Get the Resume PDF
+
 app.post("/upload",upload.single("resume"),async(req,res)=>{
-    console.log(req.file)
+    try {
+        if(!req.file){
+            return res.status(400).json({ success: false, message: "No file uploaded" });
+        
+        }
+        console.log(req.file);
+        const form = new FormData();
+
+        //Putting Data from the Form 
+
+        form.append(
+            "resume",
+            fs.createReadStream(req.file.path)
+        );
+
+        //Sending the PDF to Python Server
+
+        const response = await axios.post(
+            "http://localhost:5000/extract-skills",
+            form,
+            {
+                headers: form.getHeaders()
+            }
+        );
+
+        //Sends the Response From the Python Server To the Frontend
+
+        res.json(response.data);
+
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Failed to process resume" });
+    }
+    finally{
+        if(req.file){
+            fs.unlink(req.file.path, (err) => {
+                if (err) console.error("Failed to delete temp file:", err);
+            });
+        }
+    }
 })
 
 app.get("/",(req,res)=>{
@@ -44,8 +78,7 @@ app.get("/",(req,res)=>{
     });
 })
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+
 
 app.listen(port,()=>{
     console.log(`Server is Running on http://localhost:${port}`);
